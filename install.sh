4ce61ab67c99381a4e4a920293bd3f0eeac02597
owl <owl@tree>
2020-02-15 16:22:58 -0800

    - added granular parameterization to install script

--- install.sh
+++ install.sh
@@ -1,11 +1,37 @@
 #!/bin/sh
 
+only=0
+only_vim=0
+only_tmux=0
 install_path=~/.dotfiles
 
-while getopts "p:" option; do
-  case $option in
-    p ) install_path=$OPTARG
-    ;;
+args=`getopt p:o: $*`
+set -- $args
+
+while :; do
+  case  "$1" in
+    -p )
+      install_path=$2
+      shift; shift;
+      ;;
+    -o )
+      case $2 in
+        vim)
+          echo OPTION: $2
+          only=1
+          only_vim=1
+          ;;
+        tmux)
+          echo OPTION: $2
+          only=1
+          only_tmux=1
+          ;;
+      esac
+      shift; shift;
+      ;;
+    -- )
+      break
+      ;;
   esac
 done
 
@@ -14,6 +40,20 @@ git clone https://github.com/owlcall/dots "$install_path"
 cd "$install_path"
 git checkout master
 git pull origin master
-"$install_path/vim/activate.sh"
-"$install_path/tmux/activate.sh"
+
+echo \\nPreparing activations...\\n
+
+if [[ (only -ne 1) || (only -eq 1 && only_vim -eq 1) ]] ; then
+  echo Activating vim...
+  echo -----------------
+  "$install_path/vim/activate.sh"
+fi
+
+echo
+
+if [[ (only -ne 1) || (only -eq 1 && only_tmux -eq 1) ]] ; then
+  echo Activating tmux...
+  echo ------------------
+  "$install_path/tmux/activate.sh"
+fi
 
